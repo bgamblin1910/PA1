fdcce4b39b1c0b20e9717542b4311257a4a54d65
bgamblin1910 <123514232+bgamblin1910@users.noreply.github.com>
2023-02-01 22:46:56 -0800

    Add files via upload

---
+++ morseFunction.h
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+string translateToMorse(string letter);
+string translateToEnglish(string letter);
+string userInput();
+//changes
